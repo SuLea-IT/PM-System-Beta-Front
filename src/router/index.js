46317db8ca8691fd48b0632acb1e33d4e6d7a28d
sulea <3168162174@qq.com>
2024-08-23 09:39:12 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import GlobalLayout from '../layouts/GlobalLayout.vue'
 const routes = [
     {
         path: '/',
+        redirect: '/about',
         component: GlobalLayout,
         children: [
             {
